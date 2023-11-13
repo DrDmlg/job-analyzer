41dd3f8b13479bf1437cdac817b38d16a76c67a9
Dmitry Dorokhov <b27011971@inbox.ru>
2023-11-13 15:24:48 +0300

    feat: Create a model layer [#2]

---
+++ src/main/java/com/example/analyzer/model/Vacancy.java
@@ -0,0 +1,51 @@
+package com.example.analyzer;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Vacancy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "job_title")
+    String jobTitle;
+
+    @Column(name = "companyName")
+    String companyName;
+
+    String location;
+
+    Integer salary;
+
+    String experience;
+
+    @ElementCollection
+    @CollectionTable(name = "vacancy_technologies", joinColumns = @JoinColumn(name = "vacancy_id"))
+    @Column(name = "technology")
+    Set<String> technologies = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(name = "create_date")
+    Date createDate;
+
+    @UpdateTimestamp
+    @Column(name = "modify_date")
+    Date updateDate;
+}
