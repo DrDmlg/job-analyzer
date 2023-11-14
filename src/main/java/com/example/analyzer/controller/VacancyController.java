65aa4786f7a9bcc761f182c50f29a23da6dd5fc0
Dmitry Dorokhov <b27011971@inbox.ru>
2023-11-14 22:38:50 +0300

    config: Configure Logback for logging [#5]

--- src/main/java/com/example/analyzer/controller/VacancyController.java
+++ src/main/java/com/example/analyzer/controller/VacancyController.java
@@ -2,6 +2,9 @@ package com.example.analyzer.controller;
 
 import com.example.analyzer.model.Vacancy;
 import com.example.analyzer.service.VacancyService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/vacancies")
--- src/main/java/com/example/analyzer/service/VacancyService.java
+++ src/main/java/com/example/analyzer/service/VacancyService.java
@@ -2,12 +2,14 @@ package com.example.analyzer.service;
 
 import com.example.analyzer.model.Vacancy;
 import com.example.analyzer.repository.VacancyRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Slf4j
 @Service
 public class VacancyService {
 
@@ -27,7 +29,7 @@ public class VacancyService {
     }
 
     public List<String> analyzeTechnologies(String vacancyUrl) {
-        System.out.println(vacancyUrl);
+        log.debug(vacancyUrl);
         //:TODO
         return Arrays.asList("Java", "Spring", "Hibernate");
     }
